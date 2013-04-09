aefb8250f70b4067849ebf38bee326df133332c8
Steven Haryanto <stevenharyanto@gmail.com>
2013-04-09 15:41:01 +0000

    Import of SHARYANTO/Text-WideChar-Util-0.01 from CPAN.
    gitpan-cpan-distribution: Text-WideChar-Util
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SHARYANTO/Text-WideChar-Util-0.01.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/Text/WideChar/Util.pm
@@ -0,0 +1,274 @@
+package Text::WideChar::Util;
+
+use 5.010001;
+use locale;
+use strict;
+use utf8;
+use warnings;
+
+use List::Util qw(max);
+use Text::CharWidth qw(mbswidth);
+
+require Exporter;
+our @ISA       = qw(Exporter);
+our @EXPORT_OK = qw(
+                       mbpad
+                       mbswidth_height
+                       mbtrunc
+                       mbwrap
+               );
+
+our $VERSION = '0.01'; # VERSION
+
+sub mbswidth_height {
+    my $text = shift;
+    my $num_lines = 0;
+    my @lens;
+    for my $e (split /(\r?\n)/, $text) {
+        if ($e =~ /\n/) {
+            $num_lines++;
+            next;
+        }
+        $num_lines = 1 if $num_lines == 0;
+        push @lens, mbswidth($e);
+    }
+    [max(@lens) // 0, $num_lines];
+}
+
+sub mbwrap {
+    my ($text, $width) = @_;
+    $width //= 80;
+
+    my @res;
+    my @p = split /(\s+)/i, $text;
+    my $col = 0;
+    my $i = 0;
+    while (my $p = shift(@p)) {
+        $i++;
+        my $num_nl = 0;
+        my $is_pb; # paragraph break
+        my $is_ws;
+        my $w;
+        #say "D:col=$col, p=$p";
+        if ($p =~ /\A\s/s) {
+            $is_ws++;
+            $num_nl++ while $p =~ s/\r?\n//;
+            if ($num_nl >= 2) {
+                $is_pb++;
+                $w = 0;
+            } else {
+                $p = " ";
+                $w = 1;
+            }
+        } else {
+            $w = mbswidth($p);
+        }
+        $col += $w;
+        #say "D:col=$col, is_pb=${\($is_pb//0)}, is_ws=${\($is_ws//0)}, num_nl=$num_nl";
+
+        if ($is_pb) {
+            push @res, "\n" x $num_nl;
+            $col = 0;
+        } elsif ($col > $width+1) {
+            # remove whitespace at the end of prev line
+            if (@res && $res[-1] eq ' ') {
+                pop @res;
+            }
+
+            push @res, "\n";
+            if ($is_ws) {
+                $col = 0;
+            } else {
+                push @res, $p;
+                $col = $w;
+            }
+        } else {
+            # remove space at the end of text
+            if (@p || !$is_ws) {
+                push @res, $p;
+            } else {
+                if ($num_nl == 1) {
+                    push @res, "\n";
+                }
+            }
+        }
+    }
+    join "", @res;
+}
+
+sub mbpad {
+    my ($text, $width, $which, $padchar, $is_trunc) = @_;
+    if ($which) {
+        $which = substr($which, 0, 1);
+    } else {
+        $which = "r";
+    }
+    $padchar //= " ";
+
+    my $w = mbswidth($text);
+    if ($is_trunc && $w > $width) {
+        $text = mbtrunc($text, $width);
+    } else {
+        if ($which eq 'l') {
+            $text = ($padchar x ($width-$w)) . $text;
+        } elsif ($which eq 'c') {
+            my $n = int(($width-$w)/2);
+            $text = ($padchar x $n) . $text . ($padchar x ($width-$w-$n));
+        } else {
+            $text .= ($padchar x ($width-$w));
+        }
+    }
+    $text;
+}
+
+sub mbtrunc {
+    my ($text, $width, $return_width) = @_;
+
+    my $w = mbswidth($text);
+    die "Invalid argument, width must not be negative" unless $width >= 0;
+    return $text if $w <= $width;
+
+    # perform binary cutting
+    my @res;
+    my $wres = 0; # total width of text in @res
+    my $l = int($w/2); $l = 1 if $l == 0;
+    my $end;
+    while (1) {
+        my $left  = substr($text, 0, $l);
+        my $right = substr($text, $l);
+        my $wl = mbswidth($left);
+        #say "D: left=$left, right=$right, wl=$wl";
+        if ($wres + $wl > $width) {
+            $text = $left;
+        } else {
+            push @res, $left;
+            $wres += $wl;
+            $text = $right;
+        }
+        $l = int(($l+1)/2);
+        last if $l==1 && $end;
+        $end++ if $l==1;
+    }
+    if ($return_width) {
+        return [join("", @res), $wres];
+    } else {
+        return join("", @res);
+    }
+}
+
+1;
+# ABSTRACT: Routines for text containing wide characters
+
+
+__END__
+=pod
+
+=head1 NAME
+
+Text::WideChar::Util - Routines for text containing wide characters
+
+=head1 VERSION
+
+version 0.01
+
+=head1 SYNOPSIS
+
+ use Text::WideChar::Util qw(
+     mbpad mbswidth_height mbtrunc mbwrap);
+
+ # get width as well as number of lines
+ say mbswidth_height("red\n红色"); # => [4, 2]
+
+ # wrap text to a certain column width
+ say mbwrap("....", 40);
+
+ # pad (left, right, center) text to specified column width, handle multilines
+ say mbpad("foo", 10);                          # => "foo       "
+ say mbpad("红色", 10, "left");                 # => "      红色"
+ say mbpad("foo\nbarbaz\n", 10, "center", "."); # => "...foo....\n..barbaz..\n"
+
+ # truncate text to a certain column width
+ say mbtrunc("红色", 2 ); # => "红"
+ say mbtrunc("红色", 3 ); # => "红"
+ say mbtrunc("红red", 3); # => "红r"
+
+=head1 DESCRIPTION
+
+This module provides routines for dealing with text containing wide characters
+(wide meaning occupying more than 1 column width in terminal).
+
+=head1 FUNCTIONS
+
+=head2 mbswidth_height($text) => [INT, INT]
+
+Like L<Text::CharWidth>'s mbswidth(), but also gives height (number of lines).
+For example, C<< mbswidth_height("foobar\nb\n") >> gives [6, 3].
+
+=head2 mbwrap($text, $width) => STR
+
+Wrap text to a specified column width.
+
+C<$width> defaults to 80 if not specified.
+
+Note: currently performance is rather abysmal (~ 1200/s on my Core i5-2400
+3.1GHz desktop for a ~ 1KB of text), so call this routine sparingly ;-).
+
+=head2 mbwrap($text, $width) => STR
+
+Wrap C<$text> to C<$width> columns. It uses mbswidth() instead of Perl's
+length() which works on a per-character basis.
+
+Note: for text which does not have whitespaces between words, like Chinese, you
+will have to separate the words first (e.g. using L<Lingua::ZH::WordSegmenter>).
+The module also currently does not handle whitespace-like characters other than
+ASCII 32 (for example, the Chinese dot 。).
+
+=head2 mbpad($text, $width[, $which[, $padchar[, $truncate]]]) => STR
+
+Return C<$text> padded with C<$padchar> to C<$width> columns. C<$which> is
+either "r" or "right" for padding on the right (the default if not specified),
+"l" or "left" for padding on the right, or "c" or "center" or "centre" for
+left+right padding to center the text.
+
+C<$padchar> is whitespace if not specified. It should be string having the width
+of 1 column.
+
+=head2 mbtrunc($text, $width) => STR
+
+Truncate C<$text> to C<$width> columns. It uses mbswidth() instead of Perl's
+length(), so it can handle wide characters.
+
+Does *not* handle multiple lines.
+
+=head1 FAQ
+
+=head2 How do I truncate or pad to a certain character length (instead of column width)?
+
+You can simply use Perl's substr() which works by character.
+
+=head1 TODOS
+
+=over
+
+=back
+
+=head1 SEE ALSO
+
+L<Text::CharWidth> which provides mbswidth().
+
+L<Text::ANSI::Util> which can also handle text containing wide characters as
+well ANSI escape codes.
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
---
+++ t/01-basics.t
@@ -0,0 +1,105 @@
+#!perl -T
+
+use 5.010001;
+use strict;
+use warnings;
+use utf8;
+use constant NL => "\n";
+
+use POSIX;
+use Test::More 0.98;
+use Text::WideChar::Util qw(mbpad mbswidth_height mbwrap mbtrunc);
+
+subtest "mbswidth_height" => sub {
+    is_deeply(mbswidth_height(""), [0, 0]);
+    is_deeply(mbswidth_height("我不想回家"), [10, 1]);
+    is_deeply(mbswidth_height("我不想\n回家"), [6, 2]);
+    is_deeply(mbswidth_height("我不\n想回家\n"), [6, 3]);
+};
+
+# single paragraph
+my $txt1 = <<_;
+I dont wan't to go home. Where do you want to go? I'll keep you company. Mr Goh,
+I'm fine. You don't have to keep me company.
+_
+#qq--------10--------20--------30--------40--------50
+my $txt1w =
+qq|I dont wan't to go home. Where do you|.NL.
+qq|want to go? I'll keep you company. Mr|.NL.
+qq|Goh, I'm fine. You don't have to keep me|.NL.
+qq|company.|.NL;
+
+# multiple paragraph
+my $txt1b = <<_;
+I dont wan't to go home. Where do you want to go? I'll keep you company. Mr Goh,
+I'm fine. You don't have to keep me company.
+
+I dont wan't to go home. Where do you want to go? I'll keep you company. Mr Goh,
+I'm fine. You don't have to keep me company.
+_
+#qq--------10--------20--------30--------40--------50
+my $txt1bw =
+qq|I dont wan't to go home. Where do you|.NL.
+qq|want to go? I'll keep you company. Mr|.NL.
+qq|Goh, I'm fine. You don't have to keep me|.NL.
+qq|company.|.NL.NL.
+qq|I dont wan't to go home. Where do you|.NL.
+qq|want to go? I'll keep you company. Mr|.NL.
+qq|Goh, I'm fine. You don't have to keep me|.NL.
+qq|company.|.NL;
+
+# no terminating newline
+my $txt1c = "\x1b[31;47mI\x1b[0m dont wan't to go home. Where do you want to go? I'll keep you company. Mr Goh,
+I'm fine. You don't have to keep...";
+#qq--------10--------20--------30--------40--------50
+my $txt1cw =
+qq|\x1b[31;47mI\x1b[0m dont wan't to go home. Where do you|.NL.
+qq|want to go? I'll keep you company. Mr|.NL.
+qq|Goh, I'm fine. You don't have to keep...|;
+
+# containing wide chars
+my $txt2 = <<_;
+I dont wan't to go home. 我不想回家. Where do you want to go? I'll keep you
+company. 那你想去哪里？我陪你. Mr Goh, I'm fine. 吴先生. 我没事. You don't have
+to keep me company. 你不用陪我.
+_
+#qq--------10--------20--------30--------40--------50
+my $txt2w =
+qq|I dont wan't to go home. 我不想回家.|.NL.
+qq|Where do you want to go? I'll keep you|.NL.
+qq|company. 那你想去哪里？我陪你. Mr Goh,|.NL.
+qq|I'm fine. 吴先生. 我没事. You don't have|.NL.
+qq|to keep me company. 你不用陪我.|.NL;
+
+subtest "mbwrap" => sub {
+    is(mbwrap($txt1 , 40), $txt1w );
+    is(mbwrap($txt1b, 40), $txt1bw);
+    is(mbwrap($txt2 , 40), $txt2w );
+};
+
+subtest "mbpad" => sub {
+    my $foo = "你好吗";
+    is(mbpad("",       10),           "          ", "empty");
+    is(mbpad("你好吗", 10),           "你好吗    ", "rpad");
+    is(mbpad("你好吗", 10, "l"),      "    你好吗", "lpad");
+    is(mbpad("你好吗", 10, "c"),      "  你好吗  ", "centerpad");
+    is(mbpad("你好吗", 10, "r", "x"), "你好吗xxxx", "padchar");
+    is(mbpad("你好吗12345678", 10),   "你好吗12345678", "trunc=0");
+    is(mbpad("你好吗12345678", 10, undef, undef, 1), "你好吗1234", "trunc=1");
+};
+
+subtest "mbtrunc" => sub {
+    is(mbtrunc("我不想",  0), "");
+    is(mbtrunc("我不想",  1), "");
+    is(mbtrunc("我不想",  2), "我");
+    is(mbtrunc("我不想",  3), "我");
+    is(mbtrunc("我wo"  ,  3), "我w");
+    is(mbtrunc("我wo"  ,  4), "我wo");
+    is(mbtrunc("我不想",  4), "我不");
+    is(mbtrunc("我不想",  5), "我不");
+    is(mbtrunc("我不想",  6), "我不想");
+    is(mbtrunc("我不想", 10), "我不想");
+};
+
+DONE_TESTING:
+done_testing();
